f34b190ec305ba27c6ab0939e793e4474a36c5d3
Benjamin Herzog <mail@benchr.de>
2017-11-24 16:00:55 +0100

    Add middleware before all routes

--- Frontend-Service/src/server.js
+++ Frontend-Service/src/server.js
@@ -5,14 +5,14 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./routes/routes');
-routes(app);
 // middleware for header modification
-app.use((req, res, next) => {
-    // we need to allow all subdomains
-    res.set('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
+
+var routes = require('./routes/routes');
+routes(app);
 app.use(function (req, res) {
     res.status(404).send({ url: req.originalUrl + ' not found' });
 });

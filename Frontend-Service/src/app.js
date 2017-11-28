c8ed8615f30c21e4875d44b3ff8a2a7027346fa5
Benjamin Herzog <mail@benchr.de>
2017-11-28 09:46:06 +0100

    Inject IP address, POST to LED node

--- Frontend-Service/src/app.js
+++ Frontend-Service/src/app.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// var coapService = require("./services/coapService");
-// coapService.get('vs0.inf.ethz.ch', '/.well-known/core', console.log, console.error);
+const ipAddress = process.env.IP_ADDRESS || 'vs0.inf.ethz.ch';
+
+const coapService = require('./services/coapService');
+// coapService.get(ipAddress, '/.well-known/core', console.log, console.error);
+coapService.post(ipAddress, '/LED/green', "", undefined, console.log, console.log);
 // coapService.post('vs0.inf.ethz.ch', '/large-post', '5897', undefined, console.log, console.error);
 
 module.exports = app;

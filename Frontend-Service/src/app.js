89f12dc7c88fce1b8b824ae8f351b383b8474b3c
Jaroslav Vankat <vankatjaroslav@seznam.cz>
2017-11-29 09:41:57 +0100

    cleanup

--- Frontend-Service/src/app.js
+++ Frontend-Service/src/app.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const app = express();
 
-
-// coapService.get(ipAddress, '/.well-known/core', console.log, console.error);
-// coapService.post('vs0.inf.ethz.ch', '/large-post', '5897', undefined, console.log, console.error);
-
 module.exports = app;

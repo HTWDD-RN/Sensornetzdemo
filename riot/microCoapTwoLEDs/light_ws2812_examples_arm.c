2d4f5632cfc8af32831a11eef5fc799adbb63f83
CarlSchwedes <c.schwedes@gmx.de>
2018-01-24 10:36:07 +0100

    add_animation_pulsating_lights

--- riot/microCoapTwoLEDs/light_ws2812_examples_arm.c
+++ riot/microCoapTwoLEDs/light_ws2812_examples_arm.c
@@ -105,3 +105,65 @@ void hsv_crawling_lights(int intensity)
     xtimer_usleep(5000);
   }
 }
+
+/**
+ * Map an integer so that 0..ledstrip.len => 0..2PI
+ */
+float map2PI(int pix)
+{
+  return (PI*2.0*pix)/(float)MAXPIX;
+}
+
+/**
+ * scale values [-1.0, 1.0] to [0, 255]
+ */
+int scale(float val)
+{
+  val += 1;
+  val *= 255/2;
+  return (int)val;
+}
+
+void pulsating_light_waves(void)
+{
+  int direction = 1, pix = 0;
+
+  while(1)
+  {
+    pix++;
+
+    if(pix >= MAXPIX*13*15)
+      pix = 0;
+
+    int j = 0, offset = map2PI(pix);
+    float rsin, gsin, bsin;
+
+    if ((rand()/(float)RAND_MAX) > .995)
+      direction *= -1; // All skate, reverse direction!
+
+    for (int i = 0; i < MAXPIX; i++)
+    {
+      /**
+       * Generate some RGBs, range [-1, +1]
+       * If you think about the LED strip as a unit circle, with 
+       * circumference 2 PI, then angle between the LEDs is simply
+       *   2 PI / count 
+       * And the angle for any particular LED will be
+       *   (2 PI / count) * position
+       * That's what the map2PI() method does.
+       */
+      j = map2PI(i * direction) + offset;   // calculate angle
+      rsin = sin(j);                 // sin(t)
+      gsin = sin(2 * j / 13 + map2PI(MAXPIX / 6)); // sin(2/3 t + 1/3 PI)
+      bsin = sin(4 * j / 15 + map2PI(MAXPIX / 3)); // sin(4/5 t + 2/3 PI)
+
+      //this.ledstrip.buffer[i] = [this.scale(rsin), this.scale(gsin), this.scale(bsin)];
+      led[i].r = scale(rsin);
+      led[i].g = scale(gsin);
+      led[i].b = scale(bsin);
+    }
+
+    ws2812_sendarray((uint8_t *)&led, MAXPIX*3);
+    xtimer_usleep(50000);
+  }
+}

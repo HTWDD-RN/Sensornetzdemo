402d1bb66607ec76da8d48308f8439f287353402
Jaroslav Vankat <vankatjaroslav@seznam.cz>
2018-02-21 22:39:15 +0100

    resourceService docs

--- Frontend-Service/src/services/resourceService.js
+++ Frontend-Service/src/services/resourceService.js
@@ -26,14 +26,11 @@ var sendRequest = function (ip, actionPath, payload, success, fail) {
     return 0;
 };
 
-exports.setState = function (ip, nodeIp, actionPath, payload, success, fail) {
-    sendRequest(ip, actionPath, nodeIp + "#" + payload+ "\n", success, fail);
-};
-
 /**
- *
- * @param {string} ip
- * @param {string} actionPath
+ * Set state of multiple nodes
+ * 
+ * @param {string} ip - the border router ip
+ * @param {string} actionPath - the endpoint path
  * @param {array} data - array of objects containing node ip and payload, e.g. [{ip: '::2', payload: '1'}]
  * @param {function} success
  * @param {function} fail
@@ -47,10 +44,25 @@ exports.multicast = function (ip, actionPath, data, success, fail) {
 };
 
 /**
- *
+ * Get actions of a node.
+ * 
  * @param {String} ip - ip address of the requested node
  * @returns {Array} - raw array containing node resource information
  */
 exports.discover = function (ip, success, fail) {
     coap.get(ip, "/.well-known/core", success, fail);
+};
+
+/**
+ * Set new state of a resource (action).
+ * 
+ * @param {String} ip  - the border router ip
+ * @param {String} nodeIp - the target node ip
+ * @param {String} actionPath - the action endpoint
+ * @param {String} payload - the value to set
+ * @param {Function} success - success callback
+ * @param {Function} fail - fail callback
+ */
+exports.setState = function (ip, nodeIp, actionPath, payload, success, fail) {
+    sendRequest(ip, actionPath, nodeIp + "#" + payload+ "\n", success, fail);
 };
